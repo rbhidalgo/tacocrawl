92d0c571157b924aa6d348cfbfa40cb30c533eb9
Rogelio Hidalgo <rhidalgo987@gmail.com>
2019-07-01 15:24:35 -0700

    update map key

--- src/component/MapContainer/MapContainer.js
+++ src/component/MapContainer/MapContainer.js
@@ -23,7 +23,7 @@ class MapContainer extends Component {
 				<div style={{ height: "50vh", width: "50vw" }}>
 					<GoogleMapReact
 						bootstrapURLKeys={{
-							key: API_KEY
+							key: "AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg"
 						}}
 						defaultCenter={this.props.center}
 						defaultZoom={this.props.zoom}>
