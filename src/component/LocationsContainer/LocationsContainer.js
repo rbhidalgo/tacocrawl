6c6f9389d27f1ac4e9badee6b850df43e0ed4af7
Rogelio Hidalgo <rhidalgo987@gmail.com>
2019-08-29 19:27:09 -0700

    removed styled component from LocationsContainer.js

--- src/component/LocationsContainer/LocationsContainer.js
+++ src/component/LocationsContainer/LocationsContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import MapContainer from "../MapContainer/MapContainer";
-import CrawlContainer from "../styles/CrawlContainer";
 
 import "./locationContainer.css"
 
