405d010a41b5890dd2aa0f7480686830a3021a79
Rogelio Hidalgo <rhidalgo987@gmail.com>
2019-09-12 18:59:00 -0700

    change the href color in rest info

--- src/component/styles/RestCard.js
+++ src/component/styles/RestCard.js
@@ -9,19 +9,10 @@ margin-top: 10vh;
 height: 100vh;
 overflow-y: auto;
 
-  a:link, a:visited {
-    // background-color: #FFED03;
-    // color: black;
-    // padding: 14px 25px;
-    // text-align: center;
-    // text-decoration: none;
+.restTitle {
+  color: #049DD8;
   }
   
-  a:hover, a:active {
-    background-color: #FF009D;
-    color: white;
-    border-bottom: 2px solid yellow;
-  }
 
   .restImg {
     height: 100%;
